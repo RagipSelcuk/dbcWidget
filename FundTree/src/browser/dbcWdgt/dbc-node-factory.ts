fc4d528220e6369f61cee77dcd72fe8e80767eea
Ragip Selcuk <ragipselcuk@gmail.com>
2023-07-23 16:24:28 +0300

    Update dbc-node-factory.ts
    The code now creates a well-structured tree hierarchy with "ECU" as the top-level node, "Messages" as a main sub-tree, and "Nodes" as the other main sub-tree.

--- FundTree/src/browser/dbcWdgt/dbc-node-factory.ts
+++ FundTree/src/browser/dbcWdgt/dbc-node-factory.ts
@@ -3,123 +3,165 @@ import { ILogger } from "@theia/core";
 import { inject, injectable } from "@theia/core/shared/inversify";
 import { v4 } from "uuid";
 import { DbcEditorWidget } from "./dbc-editor-widget";
-//import { DbcLabelProvider } from "./dbc-label-provider";
 import { DbcModel } from "./dbc-model";
 
-
-
 @injectable()
-export class DbcNodeFactory implements TreeEditor.NodeFactory{
-	
-    constructor(
-     //   @inject(DbcLabelProvider) private readonly labelProvider: DbcLabelProvider,
-        @inject(ILogger) private readonly logger: ILogger) {
-    }	
-	
-	// Method to map tree data to nodes in the tree
-    mapDataToNodes(treeData: TreeEditor.TreeData): TreeEditor.Node[] {
+export class DbcNodeFactory implements TreeEditor.NodeFactory {
+  constructor(
+    @inject(ILogger) private readonly logger: ILogger
+  ) {}
 
-        const node = this.mapData(treeData.data);
-        if (node) {
-            return [node];
-        }
-        return [];
-        
-    }	
-    
-	// Method to map individual data to a node in the tree
-    mapData(data: any, parent?: TreeEditor.Node, property?: string, indexOrKey?: number | string): TreeEditor.Node {
-        if (!data) {
-            this.logger.warn('mapData called without data');
-        }
+  // Method to map tree data to nodes in the tree
+  mapDataToNodes(treeData: TreeEditor.TreeData): TreeEditor.Node[] {
+    const node = this.mapData(treeData.data);
+    if (node) {
+      return [node];
+    }
+    return [];
+  }
 
-        const node: TreeEditor.Node = {
-            ...this.defaultNode(),
-            editorId: DbcEditorWidget.WIDGET_ID,
-            name: "ECU",
-            parent: parent,
-            jsonforms: {
-                type: "ECU-Dbc",
-                data: data,
-                property: property!,
-                index: typeof indexOrKey === 'number' ? indexOrKey.toFixed(0) : indexOrKey
-            }
-        };
-
-        // containments
-        if (parent) {
-            parent.children.push(node);
-            parent.expanded = false;
-        }
-        
-       // Check if the node's data has "signals" and create child nodes for each signal
-    if (data.signals) {
-      const signals = data.signals as Array<any>;
-      signals.forEach((signal) => {
-        this.createSignalNode(signal, node);
-      });
+  // Method to map individual data to a node in the tree
+  mapData(data: any, parent?: TreeEditor.Node, property?: string, indexOrKey?: number | string): TreeEditor.Node {
+    if (!data) {
+      this.logger.warn('mapData called without data');
     }
 
-    // Recursively create child nodes for the "children" property if it exists
-    if (data.children) {
-      const children = data.children as Array<any>;
-      children.forEach((element) => {
-        this.mapData(element, node);
-      });
-    }
-    
-	if(data.nodes){
-		const nodes = data.nodes as Array <any>;
-		nodes.forEach((element) => {
-			this.mapData(element,node);
-		});
-	}
-        return node;
-    }	
-	
-	// Method to create child nodes for "signals"
-  private createSignalNode(signalData: any, parent: TreeEditor.Node) {
-    const signalNode: TreeEditor.Node = {
+    const node: TreeEditor.Node = {
       ...this.defaultNode(),
       editorId: DbcEditorWidget.WIDGET_ID,
-      name: signalData.name || "Unnamed Signal",
+      name: "ECU",
       parent: parent,
       jsonforms: {
-        type: "Signal", // Assuming "Signal" as the type for signals
-        data: signalData,
-        property: "signals",
-        index: "0", // As all signals are direct children of the message node
+        type: "ECU-Dbc",
+        data: data,
+        property: property!,
+        index: typeof indexOrKey === 'number' ? indexOrKey.toFixed(0) : indexOrKey
+      }
+    };
+
+   // Check if the node's data has "messages" and create child nodes for each message
+    if (data.children) {
+      const childrenData = data.children as Array<any>;
+      const messagesNode = this.createMessagesNode(childrenData, node);
+      node.children.push(messagesNode);
+    }
+
+    // Check if the node's data has "nodes" and create child nodes for each node
+    if (data.nodes) {
+      const nodesData = data.nodes as Array<any>;
+      const nodesNode = this.createNodesNode(nodesData, node);
+      node.children.push(nodesNode);
+    }
+
+    return node;
+  }
+private createMessagesNode(
+    childrenData: any[],
+    parent: TreeEditor.Node
+  ): TreeEditor.Node {
+    const messagesNode: TreeEditor.Node = {
+      ...this.defaultNode(),
+      editorId: DbcEditorWidget.WIDGET_ID,
+      name: "Messages",
+      parent: parent,
+      jsonforms: {
+        type: "ECU-Messages",
+        data: childrenData,
+        property: "children",
+        index: "0", // As all messages are direct children of the messages node
       },
     };
-    parent.children.push(signalNode);
+
+    childrenData.forEach((childElement, index) => {
+      const messageNode: TreeEditor.Node = {
+        ...this.defaultNode(),
+        editorId: DbcEditorWidget.WIDGET_ID,
+        name: childElement.name || "Unnamed Message",
+        parent: messagesNode,
+        jsonforms: {
+          type: "Message",
+          data: childElement,
+          property: "children",
+          index: index.toFixed(0),
+        },
+      };
+
+      // If the message has "signals", create child nodes for each signal
+      if (childElement.signals) {
+        const signalsData = childElement.signals as Array<any>;
+        signalsData.forEach((signal, signalIndex) => {
+          this.createNode(signal, messageNode, "signals", signalIndex);
+        });
+      }
+
+      messagesNode.children.push(messageNode);
+    });
+
+    return messagesNode;
   }
-  
-  
-  // Method to check if a node has creatable children	
-    hasCreatableChildren(node: TreeEditor.Node): boolean {
-        return node ? DbcModel.childrenMapping.get(node.jsonforms.type) !== undefined : false;
-    }
 
-    protected defaultNode(): Omit<TreeEditor.Node, 'editorId'> {
-        return {
-            id: v4(),
-            expanded: false,
-            selected: false,
-            parent: undefined,
-            children: [],
-            decorationData: {},
-            name: '',
-            jsonforms: {
-                type: '',
-                property: '',
-                data: undefined
-            }
-        };
-    }
+  private createNodesNode(nodesData: any[], parent: TreeEditor.Node): TreeEditor.Node {
+    const nodesNode: TreeEditor.Node = {
+      ...this.defaultNode(),
+      editorId: DbcEditorWidget.WIDGET_ID,
+      name: "Nodes",
+      parent: parent,
+      jsonforms: {
+        type: "ECU-Nodes",
+        data: nodesData,
+        property: "nodes",
+        index: "0", // As all nodes are direct children of the nodes node
+      },
+    };
 
-    /** Derives the type id from the given data. */
-  /*)  protected getTypeId(data: any): string {
-        return data && data.typeId || '';
-    }
-	*/
+    nodesData.forEach((nodeElement, index) => {
+      const nodeChild = this.mapData(nodeElement, nodesNode, "nodes", index);
+      nodesNode.children.push(nodeChild);
+    });
+
+    return nodesNode;
+  }
+
+  private createNode(
+    data: any,
+    parent: TreeEditor.Node,
+    property: string,
+    index: number
+  ) {
+    const node: TreeEditor.Node = {
+      ...this.defaultNode(),
+      editorId: DbcEditorWidget.WIDGET_ID,
+      name: data.name || "Unnamed",
+      parent: parent,
+      jsonforms: {
+        type: "Signal",
+        data: data,
+        property: property,
+        index: index.toFixed(0),
+      },
+    };
+    parent.children.push(node);
+  }
+
+  // Method to check if a node has creatable children
+  hasCreatableChildren(node: TreeEditor.Node): boolean {
+    return node ? DbcModel.childrenMapping.get(node.jsonforms.type) !== undefined : false;
+  }
+
+  protected defaultNode(): Omit<TreeEditor.Node, 'editorId'> {
+    return {
+      id: v4(),
+      expanded: false,
+      selected: false,
+      parent: undefined,
+      children: [],
+      decorationData: {},
+      name: '',
+      jsonforms: {
+        type: '',
+        property: '',
+        data: undefined
+      }
+    };
+  }
 }
