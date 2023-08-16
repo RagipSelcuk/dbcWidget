68e8dfe16fbcf8fd2aac7eafe6360fed46a5ca50
Ragip Selcuk <ragipselcuk@gmail.com>
2023-08-17 00:12:57 +0300

    Signal Detailed View started to implemented.
    Signal Detailed View started to implemented.

--- FundTree/src/browser/dbcWdgt/dbc-schema.ts
+++ FundTree/src/browser/dbcWdgt/dbc-schema.ts
@@ -1,9 +1,10 @@
 
 
+
 export const messagesView = {
   "type": "VerticalLayout",
   "elements": [
-    { // Definitions
+    { // Message Definitions
       "type": "Group",
       "label": "Definition",
       "elements": [
@@ -20,7 +21,7 @@ export const messagesView = {
             {
               "type": "Control",
               "label": 'ID',
-              "scope": "#/properties/id"
+              "scope": "#/properties/id",
             },
             {
               "type": "Control",
@@ -29,7 +30,7 @@ export const messagesView = {
             }
       ]
     },
-    { // Signals 
+    { // Message Signals 
       "type": "Group",
       "label": "Signals",
       "elements": [
@@ -40,7 +41,7 @@ export const messagesView = {
 		  }
 		]		
 	},
-    { // Transmitters 
+    { // Message Transmitters 
       "type": "Group",
       "label": "Transmitters",
       "elements": [
@@ -51,25 +52,25 @@ export const messagesView = {
 		  }
 		]		
 	},
-    { // Receivers 
+    { // Message Receivers 
       "type": "Group",
       "label": "Receivers",
       "elements": [
 		]		
 	},
-    { // Layout 
+    { // Message Layout 
       "type": "Group",
       "label": "Layout",
       "elements": [
 		]		
 	},
-    { // Attributes 
+    { // Message Attributes 
       "type": "Group",
       "label": "Attributes",
       "elements": [
 		]		
 	},
-    { // Comment 
+    { // Message Comment 
       "type": "Group",
       "label": "Comment",
       "elements": [{
@@ -88,52 +89,100 @@ export const messagesView = {
 
 
 export const signalsView = { 
-
   "type": "VerticalLayout",
   "elements": [
-    {
-      "type": "HorizontalLayout",
+    { // Signal Definitions
+      "type": "Group",
+      "label": "Definition",
       "elements": [
- {
-      "type": "Control",
-      "scope": "#/properties/endian"
+            {
+              "type": "Control",
+              "label": 'Name:',
+              "scope": "#/properties/name"
+            },
+            {
+              "type": "Control",
+              "label": 'Length[Bit]:',
+              "scope": "#/properties/length"
+            },
+            {
+              "type": "Control",
+              "label": 'Byte Order:', // Intel , Motorola
+              "scope": "#/properties/endian",
+            },
+            {
+              "type": "Control",
+              "label": 'Unit:',
+              "scope": "#/properties/unit"
+            },
+            {
+              "type": "Control",
+              "label": 'Value Type:', // Signed , Unsigned
+              "scope": "#/properties/signed"
+            },
+            {
+              "type": "Control",
+              "label": 'Factor:', 
+              "scope": "#/properties/factor"
+            },
+            {
+              "type": "Control",
+              "label": 'Offset:', 
+              "scope": "#/properties/offset"
+            },
+            {
+              "type": "Control",
+              "label": 'Minimum:', 
+              "scope": "#/properties/min"
+            },
+            {
+              "type": "Control",
+              "label": 'Maximum:', 
+              "scope": "#/properties/max"
+            },
+            {
+              "type": "Control",
+              "label": 'Value Table:', 
+              "scope": "#/properties/valueTable"
+            }
+      ]
     },
-    {
-      "type": "Control",
-      "scope": "#/properties/startBit"
-    },
-    {
-      "type": "Control",
-      "scope": "#/properties/length"
-    },
-    {
-      "type": "Control",
-      "scope": "#/properties/signed"
-    },
-    {
-      "type": "Control",
-      "scope": "#/properties/max"
-    },
-    {
-      "type": "Control",
-      "scope": "#/properties/min"
-    },
-    {
-      "type": "Control",
-      "scope": "#/properties/factor"
-    },
-    {
-      "type": "Control",
-      "scope": "#/properties/offset"
-    }    
-    
-    
-],
-    },
-    
-    // Add other controls for additional signal properties as needed
-  ]
-};
+    { // Signal Receivers
+      "type": "Group",
+      "label": "Receivers",
+      "elements": [
+		]		
+	},
+    { // Signal Attributes
+      "type": "Group",
+      "label": "Attributes",
+      "elements": [
+		]		
+	},
+    { // Signal Value Descriptions
+      "type": "Group",
+      "label": "Value Descripttions",
+      "elements": [
+		]		
+	},
+    { // Signal Comment
+      "type": "Group",
+      "label": "Comment",
+      "elements": [{
+            "type": "Control",
+            "label": "",
+	  		"scope": "#/properties/description",         
+			"options": {
+	                "multi": true // Wide Text Box uischema
+	         }            
+		  
+		}
+		]
+	}
+  ] // end of elements
+}
+
+
 
 
 
@@ -147,21 +196,22 @@ export const dbcSchema = {
       "properties": {
         "name": { "type": "string" },
         "dlc": { "type": "integer"},
-        "id": { "type": "integer" },
+        "id": { "type": "integer" }, // The ID should be displayed in Hexadecimal Format
         "sendingNode": { "type": "string" },
-        "extended": { "type": "boolean" },
+        "extended": { "type": "boolean" }, // Here, we intend to display "CAN Extended" if the value is "true"; otherwise, it should display "CAN Standard".
         "description": { "type": "string" },
 	    "signals": {
 	      "type": "array",
 	      "items": {
 	        "type": "object",
 	        "properties": {
-		        "name": { "type": "string" },
-		        "endian": { "type": "string", "enum": ["Intel", "BigEndian"] },
-		        "startBit": { "type": "integer" },
-		        "length": { "type": "integer" },
+		        "name": { "type": "string", "title": "Signal" },
+		        "startBit": { "type": "integer" , "title": "Startbit"},
+		        "length": { "type": "integer" , "title": "Length[Bit]"},
+		        "endian": { "type": "string", "enum": ["Intel", "BigEndian"], "title": "Byte Order" },
 		        "signed": {
 			      "type": "boolean",
+			      "title": "Value Type",
 			      "enum": [true, false]	// Here, we intend to display "Signed" if the value is "true"; otherwise, it should display "Unsigned".
 			    }
 	        }
